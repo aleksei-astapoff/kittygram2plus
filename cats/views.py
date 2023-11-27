aadcdc936d992c81590299817c184146e63e8c23
aleksei_astapov <Alexey.Astapoff@yandex.ru>
2023-11-27 15:37:49 +0300

    Pagination

--- cats/views.py
+++ cats/views.py
@@ -2,24 +2,28 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.throttling import AnonRateThrottle
 from rest_framework.throttling import ScopedRateThrottle
-
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Achievement, Cat, User
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
-from .permissions import OwnerOrReadOnly
 from .throttling import WorkingHoursRateThrottle
+from .pagination import CatsPagination
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-    permission_classes = (OwnerOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     # throttle_classes = (AnonRateThrottle,)
     # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
     # Если он вернёт False - все запросы будут отклонены
     throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
     # А далее применится лимит low_request
     throttle_scope = 'low_request'
+    # Даже если на уровне проекта установлен PageNumberPagination
+    # Для котиков будет работать LimitOffsetPagination
+    pagination_class = CatsPagination  # LimitOffsetPagination
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
